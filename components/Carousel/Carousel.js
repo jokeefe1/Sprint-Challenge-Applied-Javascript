4ba81ab734af4cc7a15a820b45baf42b210576e2
John O'Keefe <jokeefe@weegolobal.com>
2019-02-09 14:11:18 -0500

    initial carousel commit

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,8 @@
-class Carousel {
+// class Carousel {
 
-}
+// }
 
-let carousel = document.querySelector();
+// let carousel = document.querySelector();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +12,26 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+//Array of images
+const arr = Array.from(document.querySelectorAll('.carousel img'));
+
+//Change img display to block
+const startImg = arr[0].style.display = 'block'
+
+//Select and add event listeners to navigation buttons
+const leftNav = document
+    .querySelector('.left-button')
+    .addEventListener('click', () => handleLeftClick(arr));
+const rightNav = document
+    .querySelector('.right-button')
+    .addEventListener('click', () => handleRightClick(arr));
+
+//Handle click events on navigation buttons
+// const handleLeftClick(arr) => {
+
+// }
+
+// const handleRightClick(arr) => {
+
+// }
